ce05d4cf6f873fa1bdd91bcb6dc1f0e9856664bc
Myller Silva <myllersilva1310@gmail.com>
2022-06-21 18:37:08 -0300

    Update Node.java

--- arvore_balanceada/Node.java
+++ arvore_balanceada/Node.java
@@ -1,20 +1,18 @@
 package arvore_balanceada;
 
 public class Node {
-  Node pai;
-  Node esq;
-  Node dir;
-  int alt;
-  int valor;
-  
-  Node(int valor){
-    this.valor=valor;
+  Node pai, esq, dir;
+  int alt, valor;
+
+  Node(int valor) {
+    this.valor = valor;
+    this.alt = 0;
   }
-  
+
   @Override
   public String toString() {
-    String esq = String.format("%4s", (this.esq == null) ? null: this.esq.valor+"");
-    String dir = String.format("%4s", (this.dir == null) ? null: this.dir.valor+"");
+    String esq = String.format("%4s", (this.esq == null) ? null : this.esq.valor + "");
+    String dir = String.format("%4s", (this.dir == null) ? null : this.dir.valor + "");
     String valor = String.format("%2d", this.valor);
     return esq + " <- " + valor + " -> " + dir;
   }
