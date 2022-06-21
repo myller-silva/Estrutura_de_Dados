6a11cbe7cadb5d2db4f7fb2ab2083300c19fc297
Myller Silva <myllersilva1310@gmail.com>
2022-06-21 18:08:13 -0300

    Update Main.java

--- arvore_balanceada/Main.java
+++ arvore_balanceada/Main.java
@@ -3,9 +3,11 @@ package arvore_balanceada;
 public class Main {
   public static void main(String[] args) {
     Arvore a = new Arvore();
-
-    a.insert(25, 50, 75, 100, 125, 150, 175);
+    a.insert(10, 1, 12, 5, 23, 2, 8, 13, 9);
+    // a.insert(25, 50, 75, 100, 125, 150, 175);
+    
     System.out.println("Arvore: ");
     System.out.println(a);
+
   }
 }
