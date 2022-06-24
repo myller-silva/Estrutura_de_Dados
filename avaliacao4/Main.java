7cab7417ce99815172cf1418408a3560637269db
Myller Silva <myllersilva1310@gmail.com>
2022-06-24 11:40:38 -0300

    Update Main.java

--- avaliacao4/Main.java
+++ avaliacao4/Main.java
@@ -1,25 +1,22 @@
 package avaliacao4;
 
+import java.util.Random;
+
 public class Main {
   public static void main(String[] args) {
-    Arvore a = new Arvore();
+    // Arvore arvore = new Arvore(100, 50, 150, 75, 125, 127, 80, 53, 77, 79);
 
-    // a.insert(25, 50, 75, 100, 125, 150, 175, 200, 250, 300);
-    // int[] v = { 4, 2, 3, 1, 5, 7, 9, 6, 8, 11, 10 };
-    // int v[] = {4, 3, 2, 1, 0, -4, -1, -3, -2, -5, -6, -7};
-    int v[] = {1, 3, 2, 4, 5, 6, 9, 8, 7, 10, 11, 12};
-    a.insert(v);
-    System.out.println(a);
-    valor_e_pai(a, v);
-  }
+    Arvore arvore = new Arvore();
+    Random random = new Random();
 
-
-
-  public static void valor_e_pai(Arvore a, int v[]) {
-    for (int i : v) {
-      Node e = a.getNode(i);
-      System.out.println("valor: " + e.valor+" , pai: " + ( e.pai != null ? e.pai.valor : null ) );
+    int tam = 50;
+    for (int i = 0; i < tam; i++) {
+      int n = random.nextInt(0, tam);
+      arvore.insert(n);
     }
-    System.out.println();
+    System.out.println("Arvore:");
+    System.out.println(arvore);
+    System.out.println("root: "+arvore.root);
   }
+
 }
